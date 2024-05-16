0c5583a922305fb65dfc240b3981842cd9d24b5d
Dragos-Gabriel Capsa <d.capsa@cenit.com>
2024-05-16 15:55:26 +0000

    JQUERY

--- app/TableExport.TableView/webapp/ext/controller/ExportHandler.js
+++ app/TableExport.TableView/webapp/ext/controller/ExportHandler.js
@@ -13,13 +13,26 @@ sap.ui.define([
                 console.log(aSelectedContexts);
                 
                 console.log(this);
-                
-    
+                jQuery.ajax({
+                    url: '/service/TableExport/Export',
+                    method: 'POST',
+                    data:
+                    {},
+                    contentType: 'application/json',
+                    success: function(data) {
+                        console.log('Data from backend:', data);
+                    },
+                    error: function(error) {
+                        console.error('Error fetching data:', error);
+                    }
+                });
+
+    /*
                 const resp=await this.editFlow.invokeAction('TableExport.EntityContainer/Export',{
                     model:this.getModel()
                 });
-                
-                console.log(resp);
+                */
+                //console.log(resp);
 
              }
            
